3e28a1470c2dc6a83d979f9b5783287a3fac86ed
Tuna <tunaonair@gmail.com>
2023-06-25 12:05:15 +0300

    changed endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,13 @@ type FeedbackType = {
   rating: number;
 };
 
-/*async function getData() {
-  const res = await fetch('http://localhost:3000/api/all', { cache: 'no-store' })
+async function getData() {
+  const res = await fetch('https://feedback-system-roblox.vercel.app/api/all', { cache: 'no-store' })
   if (!res.ok) {
     throw new Error('Failed to fetch data')
   }
   return res.json()
-}*/
+}
 
 async function Home() {
   const data:FeedbackType[] = [] //await getData()
